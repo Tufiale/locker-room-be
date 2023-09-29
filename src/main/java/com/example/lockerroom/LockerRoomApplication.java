cf7194aa2b6974f79bb1a8e45c5620aee74d48bc
Tufiale <tufiale@gmail.com>
2023-09-29 15:27:46 +0100

    Initial commit

---
+++ src/main/java/com/example/lockerroom/LockerRoomApplication.java
@@ -0,0 +1,13 @@
+package com.example.lockerroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LockerRoomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LockerRoomApplication.class, args);
+	}
+
+}
